6813fd533f770ba76c54f642c8ff8eefbf932e4a
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-02-11 23:20:48 +0000

    testing between async vs sync

---
+++ test/syncBreedsTest.js
@@ -0,0 +1,24 @@
+const assert = require('chai').assert;
+const breedDetails = require('../syncBreeds');
+
+describe('#breedDetails', () => {
+  it('returns breed details for the Bombay breed', () => {
+    // We expect it to be a function, but will it be?
+    const expectedDesc = "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder.";
+    const bombay = breedDetails('Bombay');
+    assert.equal(expectedDesc, bombay);
+    // console.log(breedDetails);
+    // console.log(bombay);
+  });
+
+  it('returns undefined for empty function', () => {
+    // We expect it to be a function, but will it be?
+    const expectedDesc = undefined
+    const input = breedDetails();
+    assert.equal(expectedDesc, input);
+    // console.log(breedDetails);
+    // console.log(bombay);
+  });
+  
+});
+
